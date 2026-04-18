ec14ef0d26e45a7008447ce67c9bdffd8e1d9d60
KingStar-China <845864204@qq.com>
2026-04-18 12:24:40 +0800

    content: update 202604181224

--- src/data/search-engines.js
+++ src/data/search-engines.js
@@ -1,50 +1,50 @@
 export const searchEngines = [
   {
-    "id": "engine-1776480273264",
-    "label": "Yandex",
-    "priority": 1,
-    "placeholder": "Yandex：俄罗斯第一",
-    "urlTemplate": "https://yandex.com/search?text={query}"
-  },
-  {
-    "id": "engine-1776479358594",
-    "label": "雅虎",
-    "priority": 3,
-    "placeholder": "Yahoo：曾经我们是世界第一",
-    "urlTemplate": "https://search.yahoo.com/search?p={query}"
-  },
-  {
     "id": "engine-1774502120450",
     "label": "搜狗",
-    "priority": 4,
+    "priority": 1,
     "placeholder": "用搜狗搜索“微信公众号”",
     "urlTemplate": "https://weixin.sogou.com/weixin?type=2&s_from=input&query={query}"
   },
   {
     "id": "baidu",
     "label": "百度",
-    "priority": 5,
+    "priority": 2,
     "placeholder": "用百度搜索网页、中文内容和资讯",
     "urlTemplate": "https://www.baidu.com/s?wd={query}"
   },
   {
     "id": "google",
     "label": "Google",
-    "priority": 6,
+    "priority": 3,
     "placeholder": "用 Google 搜索全球网页和资料",
     "urlTemplate": "https://www.google.com/search?q={query}"
   },
   {
+    "id": "engine-1776480273264",
+    "label": "Yandex",
+    "priority": 4,
+    "placeholder": "Yandex：俄罗斯第一",
+    "urlTemplate": "https://yandex.com/search?text={query}"
+  },
+  {
+    "id": "engine-1776479358594",
+    "label": "雅虎",
+    "priority": 5,
+    "placeholder": "Yahoo：曾经我们是世界第一",
+    "urlTemplate": "https://search.yahoo.com/search?p={query}"
+  },
+  {
     "id": "bing",
     "label": "Bing",
-    "priority": 7,
+    "priority": 6,
     "placeholder": "用 Bing 搜索网页、图片和问答",
     "urlTemplate": "https://www.bing.com/search?q={query}"
   },
   {
     "id": "engine-1776479643699",
     "label": "Naver",
-    "priority": 9,
+    "priority": 7,
     "placeholder": "韩国第一",
     "urlTemplate": "https://search.naver.com/search.naver?&query={query}"
   }
